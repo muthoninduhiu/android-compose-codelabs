9d80ba70bdb275f0084856c8725e2a58c22fc9d1
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-16 11:05:59 +0300

    add search term and search icon

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -25,9 +25,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
@@ -48,9 +51,21 @@ fun SearchBar(
     TextField(
         value = "",
         onValueChange ={},
+        leadingIcon={
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder ={
+                     Text(stringResource(id = R.string.placeholder_search))
+        },
         modifier = modifier
-               . fillMaxWidth()
-               .heightIn(min = 56.dp)
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
 
     )
 }
