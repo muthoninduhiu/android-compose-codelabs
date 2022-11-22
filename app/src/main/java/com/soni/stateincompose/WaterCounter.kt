cc1f4669ede6dbfabe4c650193c2c0d8eb8d507b
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-22 10:06:52 +0300

    add state tracking system

--- app/src/main/java/com/soni/stateincompose/WaterCounter.kt
+++ app/src/main/java/com/soni/stateincompose/WaterCounter.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,9 +18,9 @@ fun WaterCounter(
     modifier : Modifier = Modifier
 ) {
     Column( modifier = modifier.padding(16.dp)){
-        var count = 0
+        val count: MutableState<Int> = remember{ mutableStateOf(0) }
         Text(text = "You have had $count glasses.")
-        Button(onClick = { count++ },
+        Button(onClick = { count.value++},
             Modifier.padding(top = 8.dp)) {
             Text(text = "Add One")
         }
