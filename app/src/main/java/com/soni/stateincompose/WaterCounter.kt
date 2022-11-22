bab00115bd446c63306fc144dca06380b655034d
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-22 10:12:41 +0300

    simplify the usage of count by using Kotlin's delegated properties.

--- app/src/main/java/com/soni/stateincompose/WaterCounter.kt
+++ app/src/main/java/com/soni/stateincompose/WaterCounter.kt
@@ -4,23 +4,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.soni.stateincompose.ui.theme.StateInComposeTheme
 
 @Composable
-fun WaterCounter(
-    modifier : Modifier = Modifier
-) {
+fun WaterCounter(modifier : Modifier = Modifier){
     Column( modifier = modifier.padding(16.dp)){
-        val count: MutableState<Int> = remember{ mutableStateOf(0) }
+        //val count: MutableState<Int> = remember{ mutableStateOf(0) }
+        var count by remember { mutableStateOf(0)}
         Text(text = "You have had $count glasses.")
-        Button(onClick = { count.value++},
+        Button(onClick = { count++},
             Modifier.padding(top = 8.dp)) {
             Text(text = "Add One")
         }
