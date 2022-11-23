bcafb4570a08de27f88aa632829674dca54c1db0
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-23 08:34:33 +0300

    add task items

---
+++ app/src/main/java/com/soni/stateincompose/WellnessTaskItem.kt
@@ -0,0 +1,31 @@
+package com.soni.stateincompose
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessTaskItem(
+    modifier: Modifier = Modifier,
+    onClose: () ->Unit,
+    taskItemName: String){
+    Row(
+       modifier = modifier, verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(text = taskItemName,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp))
+        IconButton(onClick =  onClose ) {
+            Icon(Icons.Filled.Close, contentDescription = "Close Icon")
+        }
+    }
+}
