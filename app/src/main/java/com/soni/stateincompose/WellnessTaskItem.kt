7068e673c295a8dd32fef67cc553d1db7e1b7a4a
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-23 09:41:01 +0300

    add task list to task item

--- app/src/main/java/com/soni/stateincompose/WellnessTaskItem.kt
+++ app/src/main/java/com/soni/stateincompose/WellnessTaskItem.kt
@@ -3,29 +3,54 @@ package com.soni.stateincompose
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WellnessTaskItem(
-    modifier: Modifier = Modifier,
-    onClose: () ->Unit,
-    taskItemName: String){
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Row(
-       modifier = modifier, verticalAlignment = Alignment.CenterVertically
-    ){
-        Text(text = taskItemName,
+        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
             modifier = Modifier
                 .weight(1f)
-                .padding(start = 16.dp))
-        IconButton(onClick =  onClose ) {
-            Icon(Icons.Filled.Close, contentDescription = "Close Icon")
+                .padding(start = 16.dp),
+            text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
         }
     }
+}
+
+@Composable
+fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        onClose = {}, // we will implement this later!
+        modifier = modifier,
+    )
 }
