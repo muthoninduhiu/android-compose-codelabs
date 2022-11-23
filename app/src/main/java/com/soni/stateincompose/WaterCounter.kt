4250093aef3b29f84d2cc2dad1db1852f1cdce86
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-23 09:38:18 +0300

    StatefulCounter owns the state. That means that it holds the count state and modifies it when calling the StatelessCounter function.

--- app/src/main/java/com/soni/stateincompose/WaterCounter.kt
+++ app/src/main/java/com/soni/stateincompose/WaterCounter.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -19,4 +22,9 @@ fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = M
             Text("Add one")
         }
     }
+}
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, { count++ }, modifier)
 }
