9b2606acc22753db7a4d7b3a15c73d8fb9014844
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-23 10:54:01 +0300

    add solution so all changes survive recomposition and configuration changes!

--- app/src/main/java/com/soni/stateincompose/WellnessTask.kt
+++ app/src/main/java/com/soni/stateincompose/WellnessTask.kt
@@ -1,3 +1,11 @@
-package com.soni.stateincompose
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
-data class WellnessTask(val id: Int, val label: String, var checked:Boolean = false)
+class WellnessTask(
+    val id: Int,
+    val label: String,
+    initialChecked: Boolean = false
+) {
+    var checked by mutableStateOf(initialChecked)
+}
