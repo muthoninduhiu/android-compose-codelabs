8800370b36fe1a2eeadc66a2632fcf3d79449e7f
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-24 10:34:11 +0300

    assigning specific colors to Material's named colors

--- ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -1,9 +1,13 @@
 package com.codelab.theming.ui.start.theme
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 @Composable
 fun JetNewsTheming(content: @Composable () -> Unit){
     MaterialTheme(content = content)
 }
+private val lightColors = lightColors(
+
+)
