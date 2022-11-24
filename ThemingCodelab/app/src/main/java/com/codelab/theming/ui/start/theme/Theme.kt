ad1144bac6b42d841855165cdea1100073d94877
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-24 10:23:24 +0300

    remove unused imports

--- ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -2,8 +2,6 @@ package com.codelab.theming.ui.start.theme
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MovableContent
-import java.lang.reflect.Modifier
 
 @Composable
 fun JetNewsTheming(content: @Composable () -> Unit){
